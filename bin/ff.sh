0235f27535342228806a060fb48052b2e16a4cc3
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-03-11 00:17:10 +0100

    missing space on fzf prompt

--- bin/ff.sh
+++ bin/ff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # the previous command in a floating popup window and store the selected directory in a variable
-path="$(fd -t d . ~ ~/.config | fzf --prompt='directory:')"
+path="$(fd -t d . ~ ~/.config | fzf --prompt='directory: ')"
 # if in a tmux session open a new tmux window with the selected directory
 
 if [[ -n "$TMUX" && -n "$path" ]]; then
